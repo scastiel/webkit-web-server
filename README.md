29acbb31684663ae5e7e4227e53e985880604bb8
Sébastien Castiel <sebastien.castiel@gmail.com>
2013-10-29 21:15:53 +0100

    Initial real commit

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ webkit-web-server
 =================
 
 Simple webserver using node-webkit.
+
+You need to install (node-webkit)[https://github.com/rogerwang/node-webkit] first.
+
+To build the application, simple zip all files to _webkit-web-server.nw_ (for example) :
+
+```
+zip webkit-web-server.nw *
+```
+
+Then open the file with the _node-webkit_ executable.
---
+++ js/webserver.js
@@ -0,0 +1,44 @@
+
+var http = require('http');
+var server;
+
+function btnStartServer_click() {
+	var host = $('#txtHost').val();
+	var port = $('#txtPort').val();
+	var content = $('#txtContent').val();
+
+	startServer(host, port, content);
+
+	$('#btnStartServer').hide();
+	$('#btnStopServer').show();
+	$('#message').html('Server running at <a target="_blank" href="http://' + host + ':'
+		+ port +'/">http://' + host + ':' + port +'/</a>').show();
+	$('#txtHost').attr('readonly', 'readlony');
+	$('#txtPort').attr('readonly', 'readlony');
+	$('#txtContent').attr('readonly', 'readlony');
+}
+
+function btnStopServer_click() {
+	stopServer();
+	
+	$('#btnStopServer').hide();
+	$('#btnStartServer').show();
+	$('#message').hide();
+	$('#txtHost').removeAttr('readonly');
+	$('#txtPort').removeAttr('readonly');
+	$('#txtContent').removeAttr('readonly');
+}
+
+function startServer(host, port, content, next) {
+	server = http.createServer((function(content) {
+		return function (req, res) {
+			res.writeHead(200, {'Content-Type': 'text/html'});
+			res.end(content);
+		};
+	})(content));
+	server.listen(port, host);
+}
+
+function stopServer() {
+	server.close();
+}
